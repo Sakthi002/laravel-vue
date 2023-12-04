f4adc744851d845730825eb9b52268fe9c5caf79
Sakthi002 <sakthiviji019@gmail.com>
2023-12-04 18:13:57 +0530

    adminlte integrated and vue router init config added

---
+++ resources/js/routes.js
@@ -0,0 +1,11 @@
+import Dashboard from "./components/Dashboard.vue";
+
+let routes = [
+    {
+        path: '/admin/dashboard',
+        name: 'admin.dashboard',
+        component: Dashboard
+    }
+]
+
+export default routes;
